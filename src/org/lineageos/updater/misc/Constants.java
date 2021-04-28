e43b20f0d00bf95fe254339c5d785da49c4606ca
Jackeagle <jackeagle102@gmail.com>
2021-04-28 04:28:08 -0400

    ApolloUpdater: Add Support to specify Apollo Variant param on API URL
    - This will allow to distinguish between Gapps and Non-gapps variants
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I29403bbb79e8c145dc2159f44d76b15822a1b86b
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -45,6 +45,7 @@ public final class Constants {
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "apollo.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "apollo.updater.uri";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
+    public static final String PROP_RELEASE_VARIANT = "ro.apollo.build.variant";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
